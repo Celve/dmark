d1c16625c2979ca5657cd34b5804a3f0f1d8c8f0
Celve <celve03@gmail.com>
2025-09-01 13:21:14 +0800

    fix: tqdm import

--- dmark/llada/only_gen.py
+++ dmark/llada/only_gen.py
@@ -6,7 +6,7 @@ from pydantic import BaseModel
 from typing import Any, Optional
 
 import torch
-import tqdm
+from tqdm import tqdm
 from transformers import AutoModel, AutoTokenizer
 from datasets import load_dataset
 

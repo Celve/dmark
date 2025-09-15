82f575907dbc59ea9099addf93e214dda52c94a9
Celve <celve03@gmail.com>
2025-09-15 12:27:33 +0800

    fix: make preprocessing faster

--- dmark/watermark/config.py
+++ dmark/watermark/config.py
@@ -17,7 +17,7 @@ class WatermarkConfig(BaseModel):
 
     def gen_green_list(self, prev_token: torch.Tensor) -> torch.Tensor:
         torch.manual_seed(prev_token.item() * self.key)
-        return torch.bernoulli(torch.full((self.vocab_size,), self.ratio)).to("cuda")
+        return torch.bernoulli(torch.full((self.vocab_size,), self.ratio, device="cuda"))
 
     def gen_bias(self, prev_token: torch.Tensor) -> torch.Tensor:
         return self.gen_green_list(prev_token) * self.delta
